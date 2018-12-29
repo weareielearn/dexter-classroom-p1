5d549c7cbe534840246470ac101b157d4317d350
= <=>
2018-12-29 16:17:08 +0530

    root ui done

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/forgot')
+def forgot():
+    return render_template('forgot.html')
+
+
+@app.route('/new')
+def forgot():
+    return render_template('signup.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
