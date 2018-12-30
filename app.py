b293c98cb42196a8c084a43464f515ee87d2285e
= <=>
2018-12-31 00:08:14 +0530

    session done for signup

--- app.py
+++ app.py
@@ -1,31 +1,63 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from operations import userAuth
 
 userAuth.user_auth_init()
 app = Flask(__name__)
+app.secret_key = 'dexter-classroom-secret-key'
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    if 'dexter_classroom_session' not in session:
+        return render_template('home.html')
+    else:
+        return redirect('/test')
 
 
 @app.route('/forgot')
 def forgot():
-    return render_template('forgot.html')
+    if 'dexter_classroom_session' not in session:
+        return render_template('forgot.html')
+    else:
+        return redirect('/')
 
 
 @app.route('/new', methods=['GET', 'POST'])
 def new():
-    if request.method == 'GET':
-        return render_template('signup.html')
+    if 'dexter_classroom_session' not in session:
+        if request.method == 'GET':
+            return render_template('signup.html')
+        else:
+            username = request.form['username']
+            password = request.form['password']
+            phone = request.form['phone']
+            community = request.form['community']
+            userAuth.create_user(username, password, phone, community)
+            session['dexter_classroom_session'] = username
+            return redirect('/')
     else:
-        username = request.form['username']
-        password = request.form['password']
-        phone = request.form['phone']
-        community = request.form['community']
-        userAuth.create_user(username, password, phone, community)
-        return"Created"
+        return redirect('/')
+
+
+@app.route('/test')
+def test():
+    if 'dexter_classroom_session' not in session:
+        return redirect('/')
+    return"Development in process"
+
+# tests
+@app.route('/test-logout')
+def test_logout():
+    if 'dexter_classroom_session' in session:
+        session.pop('dexter_classroom_session')
+    return redirect('/')
+
+
+@app.route('/test-session')
+def test_session():
+    if 'dexter_classroom_session' not in session:
+        session['dexter_classroom_session'] = 'test'
+    return redirect('/')
 
 
 if __name__ == '__main__':
